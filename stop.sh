98aa349a7fd36be0f588a9a03c8d681badcaa3b9
Christian Zufferey <christian@zufferey.com>
2026-04-16 11:04:46 +0200

    new: ajouter le script pour arrêter le container Docker

---
+++ stop.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#Petit script pour arrêter Hermes Agent
+#zf260415.1756, zf260416.1104
+
+# source: https://hermes-agent.nousresearch.com/docs/user-guide/docker
+
+
+
+docker container rm -f hermes-agent
+
+
+
